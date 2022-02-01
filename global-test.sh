9e5d24daee31d354e12bf8f8a26fa1b46a19cf66
Tobias Bülte <tobias.buelte@hbz-nrw.de>
2022-02-01 15:24:16 +0100

    Update integration test script
    Thanks to @blackwinter for suggestion.

--- global-test.sh
+++ global-test.sh
@@ -1,9 +1,1 @@
-find . -name  *.flux > fluxfiles.txt
-
-file=fluxfiles.txt
-sed -i 's/\.\///g' file
-for i in `cat $file`
-do
-  ./gradlew run --args  "$i"
-done
-
+find . -name '*.flux' -exec ./gradlew run --args {} \;
